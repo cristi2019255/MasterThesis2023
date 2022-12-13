1854545a1884ad850f5fcec60b6a44d2b5a0340e
cristi2019255 <cristi.grosu.2013@mail.ru>
2022-12-13 19:45:00 +0100

    refactoring: move tsne tools and model tools from main

--- models/VGG16.py
+++ models/VGG16.py
@@ -12,10 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from models.tools import print_model_summary
+import os
+import numpy as np
+from models.tools import freeze_layers, print_model_summary
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras import models, layers
 from utils.Logger import Logger
+from keras.models import Model
+from models.tools import freeze_layers, load_model, plot, plot_history, predict, save_history, save_model
+from keras.callbacks import ModelCheckpoint
+
+
+EPOCHS = 16
+BATCH_SIZE = 128
+MODELS_FOLDER = os.path.join("models", "model")
 
 def build_model(model_name = "MNIST"):
     console = Logger(name="VGG-16 model builder")   
@@ -59,3 +69,55 @@ def compile_model(model, loss = 'categorical_crossentropy', optimizer = 'adam', 
         print_model_summary(model, console)
     
     return model
+
+def train(X_train, Y_train, X_test, Y_test, epochs=EPOCHS, batch_size=BATCH_SIZE):
+    model = build_custom_model()
+    model = freeze_layers(model, layers_to_freeze=2)
+    model = compile_model(model)
+    plot(model)
+    
+    filepath = os.path.join(MODELS_FOLDER, model.name, f"{model.name}.h5")
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+    
+    # Fit the Model
+    history = model.fit(X_train,
+                        Y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        verbose=True,
+                        validation_data=(X_test,Y_test)
+                        )
+    save_model(model)
+    save_history(history, model.name)
+    plot_history(model.name)
+    
+    return model, history
+
+
+def get_convolution_corpus(X_train, model):
+    console = Logger("Get convolution corpus")
+    # remove last two layers
+    model1 = Model(inputs=model.input, outputs=model.layers[-3].output)
+    model1 = compile_model(model1, summary=False)
+    # get convolution corpus
+    console.log("Getting convolution corpus...")
+    convolution_corpus = model1.predict(X_train)
+    console.log(f"Convolution corpus shape: {convolution_corpus.shape}")    
+    # returns a tensor of shape (batch_size, 1, 1, 512)
+    # reshape to (batch_size, 512)
+    convolution_corpus = convolution_corpus.reshape(convolution_corpus.shape[0], convolution_corpus.shape[3])
+    console.log(f"Reshaping convolution corpus ... The new shape is: {convolution_corpus.shape}")
+    
+    # save convolution corpus
+    if not os.path.exists(os.path.join("data", model.name)):
+        os.makedirs(os.path.join("data", model.name))
+    with open(os.path.join("data", model.name, "convolution_corpus.npy"), "wb") as f:
+        np.save(f, convolution_corpus)
+    
+    return convolution_corpus
+
+
+def load_convolution_corpus(model_name):
+    with open(os.path.join("data", model_name, "convolution_corpus.npy"), "rb") as f:
+        convolution_corpus = np.load(f)
+    return convolution_corpus
