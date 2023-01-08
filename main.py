e9cc5d92aca10961bf673eee5313bb442cf9d8cb
cristi2019255 <cristi.grosu.2013@mail.ru>
2023-01-08 13:25:19 +0100

    Refactoring DBM

--- main.py
+++ main.py
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 import os
+from utils.DBM import DBM
 
 from utils.opf import opf
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # uncomment to disable GPU, run on CPU only 
 import tensorflow as tf 
-from keras import backend as K
 
 import numpy as np
 from utils.transformer import transform_mnist_to_tf_dataset
@@ -26,161 +26,22 @@ from models.VGG16 import load_convolution_corpus, train
 from models.tools import load_model, predict
 from utils.tsneTools import load_tsne_embedding, plot_tsne_embedding
 from tensorflow.keras.utils import to_categorical
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+
 
 SAMPLES_LIMIT = 5000
-DBM_RESOLUTION = 200
 
-def DBM(X_train, Y_train, X_test, Y_test, resolution = DBM_RESOLUTION):
+def d(X_train, Y_train, X_test, Y_test):
     # DBM - decision boundary mapping
     # Input: X_train, Y_train, X_test, Y_test - MNIST dataset
     # Output: A 2D image (resolution x resolution) where each pixel represents a class 
     
     # Steps: 
-    # 1. Train a model on the training data say Logistic Regression
-    # 2. Train an autoencoder on the training data (this will be used to reduce the dimensionality of the data) nD -> 2D
-    # 3. For each pixel in the 2D image, predict the class of the pixel using the trained autoencoder, the decoder part
-    # 4. Generate the 2D image
-    # 5. Plot the 2D image
-    # 6. Enjoy!
-    
-    # 1. Train a model on the training data say Logistic Regression
-    num_classes = np.unique(Y_train).shape[0]
-    
-    """
-    classifier = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(num_classes, activation=tf.nn.softmax)
-    ])
-    classifier.compile(optimizer='adam',
-                       loss='sparse_categorical_crossentropy',
-                       metrics=['accuracy'])
-    
-    classifier.build(input_shape=(None, 28, 28))
-    #classifier.summary()
-    
-    # skiping training for now
-    classifier.fit(X_train, Y_train, epochs=10)
-    classifier.save("model/DBM/MNIST/classifier.h5")
-    classifier = tf.keras.models.load_model("models/model/DBM/MNIST/classifier.h5")
-    
-    """
-    
-    # 2. Train an autoencoder on the training data (this will be used to reduce the dimensionality of the data) nD -> 2D
-    data_shape = X_train.shape[1:]
-    print(f"Data shape: {data_shape}")
-    
-    """
-    classifier = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(num_classes, activation=tf.nn.softmax)
-    ])
-    
-    encoder = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(512, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(128, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(32, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(2, activation='linear', activity_regularizer=tf.keras.regularizers.l2(1/2), bias_initializer=tf.keras.initializers.Constant(10e-4)),
-    ], name="encoder")
-    
-    decoder = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(32, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(128, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(512, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
-        tf.keras.layers.Dense(data_shape[0] * data_shape[1], activation='sigmoid'),
-        tf.keras.layers.Reshape(data_shape)
-    ], name="decoder")       
-    
-    autoencoder_classifier = tf.keras.models.Sequential([
-        encoder,
-        decoder,
-        classifier,
-    ], name="autoencoder_classifier")
-   
-    auto_encoder = tf.keras.models.Sequential([
-        encoder,
-        decoder,
-    ], name="auto_encoder")
-    
-    input = tf.keras.Input(shape=data_shape)
-    auto_encoder_classifier_output = autoencoder_classifier(input)
-    auto_encoder_output = auto_encoder(input)
-    
-    autoencoder = tf.keras.models.Model(inputs=input, outputs=[auto_encoder_output, auto_encoder_classifier_output], name="autoencoder")
-    autoencoder.compile(optimizer='adam', loss={"auto_encoder":"binary_crossentropy",
-                                               "autoencoder_classifier": "sparse_categorical_crossentropy"}, 
-                        metrics=['accuracy'])
-    
-    autoencoder.summary()
-
-    autoencoder.fit(X_train, {"auto_encoder": X_train, 
-                              "autoencoder_classifier":Y_train}, 
-                    epochs=10, batch_size=128)
-    
-    auto_encoder.save("models/model/DBM/MNIST/auto_encoder.h5")
-    autoencoder_classifier.save("models/model/DBM/MNIST/autoencoder_classifier.h5")
-    autoencoder.save("models/model/DBM/MNIST/autoencoder.h5")
-    decoder.save("models/model/DBM/MNIST/decoder.h5")
-    encoder.save("models/model/DBM/MNIST/encoder.h5")
-    classifier.save("models/model/DBM/MNIST/classifier.h5")
-    """
-    
-    autoencoder_classifier = tf.keras.models.load_model("models/model/DBM/MNIST/autoencoder_classifier.h5")
-    auto_encoder = tf.keras.models.load_model("models/model/DBM/MNIST/auto_encoder.h5")
-    classifier = tf.keras.models.load_model("models/model/DBM/MNIST/classifier.h5")
-    autoencoder = tf.keras.models.load_model("models/model/DBM/MNIST/autoencoder.h5")
-    decoder = tf.keras.models.load_model("models/model/DBM/MNIST/decoder.h5")
-    encoder = tf.keras.models.load_model("models/model/DBM/MNIST/encoder.h5")
-    
-    decoded = auto_encoder.predict(X_train)
-    decoded_labels = autoencoder_classifier.predict(X_train)
-    
-    plt.figure(figsize=(20, 6))
-    for i in range(1,20,2):
-        plt.subplot(2, 10, i)
-        plt.imshow(X_train[i], cmap='gray')
-        plt.axis('off')
-        plt.subplot(2, 10, i+1)
-        plt.imshow(decoded[i], cmap='gray')
-        plt.title(np.argmax(decoded_labels[i]))
-        plt.axis('off')
-    
-    plt.show()
-    
-    encoded = encoder.predict(X_train)
-    print("Encoded: ", encoded)
-    plt.plot(encoded[:,0], encoded[:,1], 'ro')
-    plt.show()
-    
-    min_i, max_i = np.min(encoded[:,0]), np.max(encoded[:,0])
-    min_j, max_j = np.min(encoded[:,1]), np.max(encoded[:,1])
-    
-    # 3. For each pixel in the 2D image, predict the class of the pixel using the trained autoencoder, the decoder part
-    # 4. Generate the 2D image
-    x = np.array([(i / resolution * (max_i - min_i) + min_i, j / resolution * (max_j - min_j) + min_j) for i in range(resolution) for j in range(resolution)])
-    xNd = decoder.predict(x)
-    predictions = classifier.predict(xNd)
-    #print("Predictions: ", predictions)
-    predicted_labels = np.array([np.argmax(p) for p in predictions])
-    #print("Predicted labels: ", predicted_labels)
-    img = predicted_labels.reshape((resolution, resolution))
-    print("2D boundary mapping: ", img)
-
-    # 5. Plot the 2D image
-    plt.title("2D boundary mapping")
-    plt.axis('off')
-    values = np.unique(img)
-    im =  plt.imshow(img, cmap='rainbow')
-    colors = [ im.cmap(im.norm(value)) for value in values]
-    patches = [ mpatches.Patch(color=colors[i], label=f"Value region: {values[i]}") for i in range(len(values)) ]
-    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
-
-    plt.show()
-    
-    # 6. Enjoy!
-    return img
+    # 1. Train an autoencoder on the training data (this will be used to reduce the dimensionality of the data) nD -> 2D
+    # 2. For each pixel in the 2D image, predict the class of the pixel using the trained autoencoder, the decoder part
+    # 3. Generate the 2D image
+    # 4. Plot the 2D image
+    # 5. Enjoy!
+    pass
     
 def main():
     # import MNIST dataset
@@ -234,8 +95,19 @@ def main_dbm():
     X_train /= 255
     X_test /= 255
     
-    DBM(X_train=X_train, Y_train=Y_train, X_test=X_test, Y_test=Y_test, resolution=DBM_RESOLUTION)
     
+    num_classes = np.unique(Y_train).shape[0]
+    classifier = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(num_classes, activation=tf.nn.softmax)
+    ])
+    
+    dbm = DBM(classifier)
+    dbm.generate_boundary_map(X_train, Y_train, X_test, Y_test, 
+                              train_epochs=10, 
+                              train_batch_size=128,
+                              resolution=256)
+
 if __name__ == '__main__':
     main_dbm()
         
---
+++ models/autoencoder.py
@@ -0,0 +1,190 @@
+# Copyright 2023 Cristian Grosu
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def build_autoencoder(classifier, input_shape = (28, 28), num_classes = 10, show_summary = False):
+    """ Building an autoencoder for dimensionality reduction of the data
+
+    Args:
+        classifier (keras.Model): The classifier to be used for the autoencoder
+        input_shape (tuple, optional): Data input shape. Defaults to (28, 28).
+        num_classes (int, optional): Target number of classes to be predicted. Defaults to 10.
+        show_summary (bool, optional): If true shows the autoencoder summary. Defaults to False.
+
+    Returns:
+        keras.Model: The autoencoder model
+    """
+    encoder = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(128, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(32, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(2, activation='linear', activity_regularizer=tf.keras.regularizers.l2(1/2), bias_initializer=tf.keras.initializers.Constant(10e-4)),
+    ], name="encoder")
+    
+    decoder = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(32, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(128, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(512, activation='relu', bias_initializer=tf.keras.initializers.Constant(10e-4)),
+        tf.keras.layers.Dense(input_shape[0] * input_shape[1], activation='sigmoid'),
+        tf.keras.layers.Reshape(input_shape)
+    ], name="decoder")       
+    
+    
+    input = tf.keras.Input(shape=input_shape, name="input")
+    
+    autoencoder = Autoencoder(encoder, decoder, classifier, input)
+    
+    if show_summary:
+        autoencoder.summary()
+        
+    return autoencoder
+
+def load_autoencoder(folder_path = os.path.join("models", "model", "DBM", "MNIST")):
+    try:
+        return Autoencoder(folder_path = folder_path, load = True)
+    except Exception as e:
+        print(e)
+        raise e
+
+class Autoencoder:
+    def __init__(self, encoder = None, decoder = None, classifier = None, input_layer = None, 
+                 folder_path = os.path.join("models", "model", "DBM", "MNIST"), load = False):
+        """
+            Creates an autoencoder model.
+            Encoder: The encoder part of the autoencoder.
+            Decoder: The decoder part of the autoencoder.
+            Classifier: The classifier part of the autoencoder.
+            Input_layer: The input layer of the autoencoder.
+        """
+        self.save_folder_path = folder_path
+        self.encoder = encoder
+        self.decoder = decoder
+        self.classifier = classifier
+        self.input_layer = input_layer
+        
+        if load:
+            self.load()
+        else:
+            self.build()
+    
+    def load(self, folder_path = os.path.join("models", "model", "DBM", "MNIST")):
+        """
+            Loads an autoencoder from the specified folder path. With the .h5 extension.
+        """
+        self.save_folder_path = folder_path
+        try:
+            self.autoencoder_classifier = tf.keras.models.load_model(os.path.join(folder_path, "autoencoder_classifier.h5"))
+            self.auto_encoder = tf.keras.models.load_model(os.path.join(folder_path, "auto_encoder.h5"))
+            self.classifier = tf.keras.models.load_model(os.path.join(folder_path, "classifier.h5"))
+            self.autoencoder = tf.keras.models.load_model(os.path.join(folder_path, "autoencoder.h5"))
+            self.decoder = tf.keras.models.load_model(os.path.join(folder_path, "decoder.h5"))
+            self.encoder = tf.keras.models.load_model(os.path.join(folder_path, "encoder.h5"))
+        except Exception as e:
+            print("Autoencoder not found. Please check the path folder and make sure the autoencoder is saved there")        
+            print(f"Exception: {e}")
+            raise e
+    
+    
+    def build(self):
+        self.autoencoder_classifier = tf.keras.models.Sequential([
+            self.encoder,
+            self.decoder,
+            self.classifier,
+        ], name="autoencoder_classifier")
+    
+        self.auto_encoder = tf.keras.models.Sequential([
+            self.encoder,
+            self.decoder,
+        ], name="auto_encoder")
+        
+        auto_encoder_classifier_output = self.autoencoder_classifier(self.input_layer)
+        auto_encoder_output = self.auto_encoder(self.input_layer)
+        
+            
+        self.autoencoder = tf.keras.models.Model(inputs=self.input_layer, 
+                                            outputs=[auto_encoder_output, 
+                                                    auto_encoder_classifier_output], 
+                                            name="autoencoder")
+        
+        self.autoencoder.compile(optimizer='adam', 
+                                loss={"auto_encoder":"binary_crossentropy",
+                                      "autoencoder_classifier": "sparse_categorical_crossentropy"}, 
+                                metrics=['accuracy'])
+    
+    def summary(self):
+        self.autoencoder.summary()
+        
+    def fit(self, x_train, y_train, x_test, y_test, epochs = 10, batch_size = 128):
+        """ Fits the model to the specified data.
+
+        Args:
+            x_train: Train input values
+            y_train: Train target values
+            x_test: Test input values
+            y_test: Test target values
+            epochs (int, optional): The number of epochs. Defaults to 10.
+            batch_size (int, optional): Data points used for one batch. Defaults to 128.
+        """
+        self.autoencoder.fit(x_train, [x_train, y_train], 
+                            epochs=epochs, 
+                            batch_size=batch_size, 
+                            shuffle=True, 
+                            validation_data=(x_test, [x_test, y_test]))
+        
+        self.save()
+        
+    def save(self):
+        """
+            Saves the model to the specified folder path. With the .h5 extension.
+        """       
+        folder_path = self.save_folder_path 
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            
+        self.auto_encoder.save(os.path.join(folder_path, "auto_encoder.h5"))
+        self.autoencoder_classifier.save(os.path.join(folder_path, "autoencoder_classifier.h5"))
+        self.autoencoder.save(os.path.join(folder_path, "autoencoder.h5"))
+        self.decoder.save(os.path.join(folder_path, "decoder.h5"))
+        self.encoder.save(os.path.join(folder_path, "encoder.h5"))
+        self.classifier.save(os.path.join(folder_path, "classifier.h5"))
+    
+    def show_predictions(self, data, labels):
+        decoded = self.auto_encoder.predict(data)
+        decoded_labels = self.autoencoder_classifier.predict(data)
+        predicted_labels = [np.argmax(label) for label in decoded_labels]
+        
+        
+        plt.figure(figsize=(20, 6))
+        
+        for i in range(1,20,2):
+            plt.subplot(2, 10, i)
+            plt.imshow(data[i], cmap='gray')
+            plt.axis('off')
+            plt.subplot(2, 10, i+1)
+            plt.imshow(decoded[i], cmap='gray')
+            plt.title(f"Predicted: {predicted_labels[i]} \n Actual: {labels[i]}", color='green' if predicted_labels[i] == labels[i] else 'red')
+            plt.axis('off')
+        
+        plt.show()
+    
+    def encode(self, data):
+        return self.encoder.predict(data)
+    
+    def decode(self, data):
+        return self.decoder.predict(data)
