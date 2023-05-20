26b07a5dce3c58254171d43c6f05d116c2f997fe
Cristian Grosu <cristian.grosu@financiallease.nl>
2023-05-20 12:20:26 +0200

    DEV-13 add experiments instructions in mainexperiment.py

--- mainexperiment.py
+++ mainexperiment.py
@@ -15,11 +15,60 @@
 from experiments import resolutions_run_times, compute_errors, resolutions_experiment_plot, errors_plot
 
 # ---------------------------------------------------
+# INSTRUCTIONS:
+#
+# WARNING: The experiments need to be run in the following order: 1, 2, 3, 4, ...
+# 
+# 
+# EXPERIMENT 1: (Run the experiments for resolutions with a specific fast decoding strategy)
+#     1. Set the FAST_DECODING_STRATEGY in the experiments/scripts/experiment_resolutions.py file
+#     1.1. (Optional) Set the RESOLUTION_RANGE in the experiments/scripts/experiment_resolutions.py file
+#     2. Run resolutions_run_times() from this file
+#     2.1 WARNING: If the experiment was already run, the results will not be overwritten
+#     2.2 WARNING: If you want to run the experiment again, please delete the folder: experiments/results/{dataset_name}/{dbm_strategy}/({projection})/
+#     2.3 WARNING: This experiment is time consuming (it can take up to 1 hour) :))
+# OUTPUT: The results will be saved in the folder experiments/results/{dataset_name}/{dbm_strategy}/({projection})/{FAST_DECODING_STRATEGY}/
+#
+# IMPORTANT: Repeat EXPERIMENT 1 for all the fast decoding strategies
+#
+# EXPERIMENT 2: (Compute a plot to compare the fast decoding strategies run times)
+#     1. Run resolutions_experiment_plot() from this file
+# OUTPUT: The plot will be saved in the folder experiments/results/{dataset_name}/{dbm_strategy}/({projection})/resolutions_experiment.png
+
+# EXPERIMENT 3: (Compute the errors for the different resolutions)
+#     1. Run compute_errors() from this file
+# OUTPUT: The errors results will be saved for each fast decoding strategy in the folder: 
+#         experiments/results/{dataset_name}/{dbm_strategy}/({projection})/{FAST_DECODING_STRATEGY}/errors_results.txt
+#
+# EXPERIMENT 4: (Compute a plot to compare the errors for the different resolutions)
+#     1. Run errors_plot() from this file
+# OUTPUT: The plot will be saved in the folder experiments/results/{dataset_name}/{dbm_strategy}/({projection})/errors_experiment.png
+#
+# DEFINITION: EXPERIMENT 1-4 is called a EXPERIMENT PROJECTION
+#
+# EXPERIMENT 5: Run EXPERIMENT PROJECTION for all the projections (t-SNE, PCA, UMAP)
+#
+# DEFINITION: EXPERIMENT 5 is called a EXPERIMENT DBM STRATEGY
+# EXPERIMENT 6: Run EXPERIMENT DBM STRATEGY for all the dbm strategies (DBM, SDBM)
+#
+# DEFINITION: EXPERIMENT 6 is called a EXPERIMENT DATASET
+# EXPERIMENT 7: Run EXPERIMENT DATASET for all the datasets (MNIST, Fashion-MNIST, CIFAR-10, [placeholder for parasites dataset])
+#
+# ETA (resolutions run times): 1h (strategy) * 4 (strategies) * (3 + 1) (3 projections + SDBM) * 4 (datasets) = 64h = 2.6 days
+# ETA (errors): 1h
+# ETA (plots): 1h
+# ETA (confidence maps, different interpolation methods): 1h (strategy) * 3 (interpolation methods) * 3 (strategies) * (3 + 1) (3 projections + SDBM) * 4 (datasets) = 108h = 4.5 days
+#
+# TOTAL ETA: 8.1 days (with 3 days of buffer) = 11.1 days = 2 weeks of continuous work
+# ---------------------------------------------------
+
+
+# ---------------------------------------------------
 #                 RUN THE EXPERIMENT(S)
 # ---------------------------------------------------
 if __name__ == "__main__":
-   #resolutions_run_times()
+   resolutions_run_times()
    #resolutions_experiment_plot()
    #compute_errors()
-   errors_plot()
+   #errors_plot()
    
