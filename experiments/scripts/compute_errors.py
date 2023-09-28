DEV-13 fix the confidence border calculation

experiments/scripts/compute_errors.py
@@ -16,7 +16,7 @@ import os
 import numpy as np
 
 from .config import CONFIDENCE_FOLDER_NAME, IMG_FOLDER_NAME, INTERPOLATION_FOLDER_NAME, IMG_ERRORS_RESULTS_FILE_NAME, CONFIDENCE_ERRORS_RESULTS_FILE_NAME
-from .utils import compute_error
+from .utils import compute_confidence_error, compute_error
 
 RESULTS_FOLDER = os.path.join("experiments", "results", "MNIST", "DBM", "t-SNE")
 
@@ -69,7 +69,7 @@ def compute_confidence_errors(folder=RESULTS_FOLDER, interpolation_method='linea
         #    continue
         
         with open(errors_file_path, "w") as f:
-            f.write("RESOLUTION,ERROR,ERROR RATE\n")
+            f.write("RESOLUTION,ERROR\n")
         
         # get all the images in the folder/img
         confidence_names = os.listdir(os.path.join(folder, dir, INTERPOLATION_FOLDER_NAME, interpolation_method))
@@ -83,8 +83,8 @@ def compute_confidence_errors(folder=RESULTS_FOLDER, interpolation_method='linea
             with open(ground_truth_conf_img_path, "rb") as f:
                 ground_truth_conf_img = np.load(f)
             # computing the error
-            error, error_rate = compute_error(conf_img, ground_truth_conf_img, comparing_confidence=True)  
-            line = f"{resolution},{str(error)},{str(error_rate)}\n"
+            error = compute_confidence_error(conf_img, ground_truth_conf_img)  
+            line = f"{resolution},{str(error)}\n"
             with open(errors_file_path, "a") as file:
                 file.write(line)
     
@@ -96,7 +96,7 @@ def compute_confidence_errors_for_confidence_interpolation(folder=RESULTS_FOLDER
     
     errors_file_path = os.path.join(folder, "confidence_interpolation", CONFIDENCE_ERRORS_RESULTS_FILE_NAME)
     with open(errors_file_path, "w") as f:
-            f.write("RESOLUTION,ERROR,ERROR RATE\n")
+            f.write("RESOLUTION,ERROR\n")
       
     confidence_names = os.listdir(conf_interpolation_folder)
     for conf_img_name in confidence_names:
@@ -108,8 +108,8 @@ def compute_confidence_errors_for_confidence_interpolation(folder=RESULTS_FOLDER
             ground_truth_conf_img = np.load(f)
         
         resolution = conf_img_name.split(".")[0]   
-        error, error_rate = compute_error(conf_img, ground_truth_conf_img, comparing_confidence=True)  
-        line = f"{resolution},{str(error)},{str(error_rate)}\n"
+        error = compute_confidence_error(conf_img, ground_truth_conf_img)  
+        line = f"{resolution},{str(error)}\n"
         with open(errors_file_path, "a") as file:
             file.write(line)
     

experiments/scripts/utils.py
@@ -78,21 +78,24 @@ def import_dbm(dbm_technique=DBM_TECHNIQUE, classifier_path=CLASSIFIER_PATH):
     dbm = DBM_TECHNIQUES[dbm_technique](classifier)
     return dbm
 
-def compute_error(img1, img2, comparing_confidence=False):
+def compute_error(img1, img2):
     errors = 0
     for i in range(img1.shape[0]):
         for j in range(img1.shape[1]):
             if img1[i, j] != img2[i, j]:
-                if comparing_confidence:
-                    if abs(img1[i, j] - img2[i, j]) > 0.03:
-                        errors += 1
-                else:
-                    errors += 1
+                errors += 1
 
     errors_rate: float = errors / (img1.shape[0] * img1.shape[1]) * 100
     #print("Error: ", errors)
     #print("Error rate: ", errors_rate, "%")
     return errors, round(errors_rate, 3)
+
+def compute_confidence_error(approximated_signal, original_signal):
+    # sum ((x_i - x^_i)^2) / sum (x^_i)^2
+    #return np.sum((original_signal - approximated_signal) ** 2) / np.sum(approximated_signal ** 2)
+    
+    # sum ((x_i - x^_i)^2) / sum (x_i)^2
+    return np.sum((original_signal - approximated_signal) ** 2) / np.sum(original_signal ** 2)
 
 
 def save_result(path, result):

src/decision_boundary_mapper/DBM/tools.py
@@ -218,21 +218,30 @@ def get_projection_errors_using_inverse_projection(Xnd: np.ndarray, X2d: np.ndar
 def generate_windows(window_size: int, initial_resolution: int, resolution: int = 1024):
 
     indexes = [((i * window_size + window_size / 2 - 0.5), (j * window_size + window_size / 2 - 0.5)) for i in range(initial_resolution) for j in range(initial_resolution)]
+    
+    border_indexes = [(-1,-1), (-1, resolution), (resolution, -1), (resolution, resolution)]
+    for i in range(initial_resolution):
+        p = (i * window_size + window_size / 2 - 0.5)
+        border_indexes += [(-1, p), (p, -1), (p, resolution), (resolution, p)]
+    
     sizes = [(window_size, window_size)] * len(indexes)
     if initial_resolution * window_size == resolution:
-        return indexes, sizes, initial_resolution
+        return indexes, sizes, border_indexes
     
     slack = (resolution - initial_resolution * window_size) / 2
-        
-    indexes += [(i * window_size + window_size / 2 - 0.5, resolution - slack - 0.5) for i in range(initial_resolution)]
-    sizes += [(window_size, (resolution - initial_resolution * window_size))] * initial_resolution
-    indexes += [(resolution - slack - 0.5, i * window_size + window_size / 2 - 0.5) for i in range(initial_resolution)]
+    p = resolution - slack - 0.5
+       
+    indexes += [(i * window_size + window_size / 2 - 0.5, p) for i in range(initial_resolution)]
+    sizes += [(window_size, (resolution - initial_resolution * window_size))] * initial_resolution 
+    indexes += [(p, i * window_size + window_size / 2 - 0.5) for i in range(initial_resolution)]
     sizes += [((resolution - initial_resolution * window_size), window_size)] * initial_resolution
-    indexes += [(resolution - slack - 0.5, resolution - slack - 0.5)]
+    
+    indexes += [(p, p)]
     sizes += [((resolution - initial_resolution * window_size), (resolution - initial_resolution * window_size))]
-    initial_resolution += 1
-        
-    return indexes, sizes, initial_resolution
+    
+    border_indexes += [(-1, p), (p, -1), (p, resolution), (resolution, p)]
+    
+    return indexes, sizes, border_indexes
 
 @njit
 def get_window_borders(x, y, w, h):

test.py
@@ -19,6 +19,8 @@ import os
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from src.decision_boundary_mapper.DBM import SDBM
+from src.decision_boundary_mapper.DBM.SDBM.SDBM import NNArchitecture
 from src.decision_boundary_mapper.utils.dataReader import import_mnist_dataset, import_folder_dataset
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
@@ -55,23 +57,6 @@ def import_2d_data():
     with open(os.path.join("tmp", "MNIST", "DBM", "t-SNE", "test_2d.npy"), "rb") as f:
         X2d_test = np.load(f)
     return X2d_train, X2d_test
-
-
-def compare_images(img1, img2, comparing_confidence=False):
-    errors = 0
-    for i in range(img1.shape[0]):
-        for j in range(img1.shape[1]):
-            if img1[i, j] != img2[i, j]:
-                if comparing_confidence:
-                    if abs(img1[i, j] - img2[i, j]) > 0.03:
-                        errors += 1
-                else:
-                    errors += 1
-
-    print("Errors: ", errors)
-    print("Error rate: ", errors / (img1.shape[0] * img1.shape[1]) * 100, "%")
-    return errors
-
 
 def test():
     
@@ -124,22 +109,6 @@ def test():
         np.save(f, img1)
     with open(img_confidence_path, "wb") as f:
         np.save(f, img_confidence1)
-
-def test2():
-    with open("img1.npy", "rb") as f:
-        img1 = np.load(f)
-    with open("img2.npy", "rb") as f:
-        img2 = np.load(f)
-    with open("img_confidence1.npy", "rb") as f:
-        img_confidence1 = np.load(f)
-    with open("img_confidence2.npy", "rb") as f:
-        img_confidence2 = np.load(f)
-
-    print("Comparing dbm images...")
-    compare_images(img1, img2)
-    print("Comparing confidence images...")
-    compare_images(img_confidence1, img_confidence2, comparing_confidence=True)
-
 
 def test3():
     X_train, X_test, Y_train, Y_test = import_data()
@@ -382,7 +351,7 @@ def train_mnist_classifier():
     classifier = tf.keras.Sequential([
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(10, activation=tf.keras.activations.softmax,
-                              kernel_initializer=tf.keras.initializers.HeNormal(42))
+                              kernel_initializer=tf.keras.initializers.RandomNormal(42))
     ])
 
     classifier.compile(
@@ -390,21 +359,80 @@ def train_mnist_classifier():
     classifier.fit(X_train, Y_train, epochs=20, batch_size=32, shuffle=False)
 
     classifier.evaluate(X_test, Y_test)
-    classifier.save(os.path.join(
-        "tmp", "MNIST", "classifier"), save_format="tf")
+    classifier.save(os.path.join("tmp", "MNIST", "classifier"), save_format="tf")
+
+def compute_dbm_confidence_interpolation():
+    X_train, X_test, Y_train, Y_test = import_data()
     
+    resolution = 500
+    classifier = import_classifier()
+    dbm = DBM(classifier)
+    
+    X2d_train, X2d_test = import_2d_data()
+        
+    dbm.generate_boundary_map(X_train,
+                              X_test,
+                              X2d_train,
+                              X2d_test,
+                              load_folder=os.path.join("tmp", "MNIST", "DBM"),
+                              resolution=10
+                              )
+
+    img, img_confidence, _ = dbm._get_img_dbm_fast_confidence_interpolation_strategy(resolution, initial_resolution=32)
+
+    with open(str(resolution) + "_img.npy", "wb") as f:
+        np.save(f, img)
+    with open(str(resolution) + "_conf.npy", "wb") as f:
+        np.save(f, img_confidence)
+        
+    # img, img_confidence, _ = dbm._get_img_dbm_(resolution)
+    # with open(str(resolution) + "_true_img.npy", "wb") as f:
+    #     np.save(f, img)
+    # with open(str(resolution) + "_true_conf.npy", "wb") as f:
+    #     np.save(f, img_confidence)
+
+
+def test4():
+    res = 500
+    
+    # true_img_path = f"experiments/results/MNIST/DBM/t-SNE/none/img/{res}.npy"
+    # true_img_conf_path = f"experiments/results/MNIST/DBM/t-SNE/none/confidence/{res}.npy"
+    
+    true_img_path = f"{res}_true_img.npy"
+    true_img_conf_path = f"{res}_true_conf.npy"
+
+    
+    with open(f"{res}_img.npy", "rb") as f:
+        interpolated_img = np.load(f)
+    with open(true_img_path, "rb") as f:
+        ground_truth = np.load(f)
+    
+    err = 0
+    for i in range(res):
+        for j in range(res):
+            if interpolated_img[i][j] != ground_truth[i][j]:
+                err += 1
+    print(err)
+    print(err / (res **2))
+
+
+    with open(f"{res}_conf.npy", "rb") as f:
+        interpolated_img = np.load(f)
+    with open(true_img_conf_path, "rb") as f:
+        ground_truth = np.load(f)
+    
+    print(np.sum((ground_truth - interpolated_img) ** 2) / np.sum(ground_truth ** 2))
+
 #show_bilinear_interpolation()
 #show_grid()
 #show_fig_3_10()
-
 #show_img("/Users/cristiangrosu/Desktop/code_repo/MasterThesis2023/experiments/results/MNIST/SDBM/none/img/200.npy")
 #import_folder_dataset("./data/parasites_focus_plane_divided/larvas/resized")
-
 #test()
 #show_errors()
+#test3()
+#test_interpolation()
+train_mnist_classifier()
 
-# test2()
-# test3()
-# test_interpolation()
-
-#train_mnist_classifier()
+#compute_dbm_confidence_interpolation()
+#test4()