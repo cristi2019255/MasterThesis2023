36a51b104b211d9a49b79b4cf4cde4a5bb2b9d88
Cristian Grosu <cristian.grosu@financiallease.nl>
2023-10-19 17:44:36 +0200

    DEV-13 release 0.7.1 with data labels view in DBM gui

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.system("pdoc --html src/decision_boundary_mapper -o docs")
 
 setup(
     name='decision-boundary-mapper',
-    version='0.7.0',
+    version='0.7.1',
     license='MIT',
     author="Cristian Grosu",
     author_email='c.grosu@students.uu.nl',
